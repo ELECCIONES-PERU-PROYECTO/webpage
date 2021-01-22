4c0bfb89dd7fda9906c04b9eb4eb5cf487f9c42a
gracenikole <grace.alvarado@utec.edu.pe>
2021-01-22 13:55:52 -0500

    wno

--- elecciones_peru/elecciones/static/js/filter.js
+++ elecciones_peru/elecciones/static/js/filter.js
@@ -1,12 +1,23 @@
 let dni = 17903382
 
-win_href = window.location.hostname
+let win_href = window.location.hostname
 
-function get_nivel_academico(){
+function get_nivel_academico() {
   let nivel_academico = document.getElementById("nivel_academico")
   let nivel_academico_value = nivel_academico.options[nivel_academico.selectedIndex].value;
-  // location.replace('https://' + win_href + ':8000/elecciones/candidatos/estudios/' + nivel_academico_value)
+  // location.replace('http://' + win_href + ':8000/elecciones/candidatos/estudios/' + nivel_academico_value)
+  return nivel_academico_value
 }
+
+function cargos_previos() {
+  let anhio_servicios = document.getElementsByName("anhio_servicio");
+  for(let i = 0; i < anhio_servicios.length; i++) {
+    if(anhio_servicios[i].checked == true) 
+      return anhio_servicios[i].value
+  }
+}
+
+
 
 function filter() {
   // get_range_edad()
--- elecciones_peru/elecciones/static/js/serv.js
+++ elecciones_peru/elecciones/static/js/serv.js
@@ -18,7 +18,6 @@ async function getCandidato() {
   }
 }
 
-
 async function grado_esudios() {
   try {
     const response = await axios.get('/candidatos/estudios/?niv_acad=' + nivel_academico_value)
@@ -27,3 +26,31 @@ async function grado_esudios() {
     console.error(error)
   }
 }
+
+filters: [
+  {"key" : "city", "value" : "chicago"},  
+  {"key" : "city", "value" : "ohio"},
+ ]
+
+async function GET_USERS() {
+
+  // Since we have mixed filters as objects in an array
+  // we must map them creating a new array with their values
+   const cityParams = state.filters.map((filter) =>
+        filter.key === 'city' ? filter.value : undefined
+      )
+  
+  const ageParams = state.filters.map((filter) =>
+        filter.key === 'age' ? filter.value : undefined
+      )
+  
+    const data = await this.$axios.get('/users', {
+      params: {
+        city: cityParams,
+        age: ageParams
+      },
+      paramsSerializer: (params) => {
+        return qs.stringify(params, { arrayFormat: 'repeat' })
+      },
+    })
+  }
