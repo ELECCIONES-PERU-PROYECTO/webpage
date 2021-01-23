e6c4f98a38c1ae5d7bcac5cd76888e66c6840064
Miunmn <esteban.principe@utec.edu.pe>
2021-01-23 13:17:12 -0500

    Update filter.js dashboard.html

--- elecciones_peru/elecciones/static/js/filter.js
+++ elecciones_peru/elecciones/static/js/filter.js
@@ -1,3 +1,4 @@
+/* ---------------Globlal Variables-----------*/
 let dni = 17903382
 
 let win_href = window.location.hostname
@@ -15,14 +16,51 @@ let filtro_cargo_postula = false
 let filtro_organizacion = false
 let filtro_distrito = false
 
+/* Variables de filtros*/
+ //Nivel Academico
+  let nivel_academico = ""
+ //Cargos previos
+  let cargos_previos_order = ""
+ //Sentencias
+  let s = ""
+   
+
+
+
+
+ //Bienes y Rentas
+  //let cant_ingresos = ""
+  //let cant_inmuebles = ""
+  let valor_inmueble = ""
+  let cant_muebles = ""
+  let valor_mueble = ""
+  
+  //Renuncias
+   
+
+/*                      */
+/* Contadores de tipo de queries */
+//let cant_filtros_normales = 0
+//let cant_filtros_ob = 0
+let cant_filtro_especial = 0
+/*-------------------------------*/
+
+/*     QUERY  -> PRODUCTO FINAL    */
+let query = ""
+/* ---------------Globlal Variables-----------*/
+
 /* Onclick Academico */
+
+
 
 function onclick_cargo_popular(){
   if(filtro_cargo == false){
     filtro_cargo = true
     cant_filtros++
+    cant_filtros_normales++
+    console.log("cant_filtros_normales: " ,cant_filtros_normales)
+    console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
     console.log("cant_filtros: " ,cant_filtros)
-
   }
 }
 function quitar_seleccion_cargo(){
@@ -34,6 +72,9 @@ function quitar_seleccion_cargo(){
     button_desc.checked = false
     filtro_cargo = false
     cant_filtros--
+    cant_filtros_normales--
+    console.log("cant_filtros_normales: " ,cant_filtros_normales)
+    console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
     console.log("cant_filtros: " ,cant_filtros)
   }
 }
@@ -51,13 +92,32 @@ function quitar_seleccion_b_r(){
     document.getElementById("valor_mueble_asc").checked = false
     document.getElementById("valor_mueble_desc").checked = false
     cant_filtros--
+    cant_filtros_ob--
+    console.log("cant_filtros_ob: " ,cant_filtros_ob)
+    console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
     console.log("cant_filtros: " ,cant_filtros)
     filtro_bienesraices = false
   }
 }
+
+
+function quitar_seleccion_sentencias(){
+
+}
+function quitar_seleccion_distrito(){
+
+}
+
+function quitar_seleccion_postula(){
+
+}
+
 function quitar_seleccion_edad(){
   if(filtro_edad == true){
     cant_filtros--
+    cant_filtros_ob--
+    console.log("cant_filtros_ob: ", cant_filtros_ob)
+    console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
     console.log("cant_filtros: " ,cant_filtros)
     filtro_edad = false
     let inputs = document.getElementsByName("rango_edad")
@@ -71,6 +131,9 @@ function quitar_seleccion_org_politica(){
   if(filtro_organizacion == true){
     cant_filtros--
     console.log("cant_filtros: " ,cant_filtros)
+    cant_filtros_normales--
+    console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
+    console.log("cant_filtros_normales: " ,cant_filtros_normales) 
     filtro_organizacion = false
     let select_ = document.getElementById("select_org_politica")
     select_.selectedIndex = 0
@@ -80,7 +143,10 @@ function quitar_seleccion_org_politica(){
 function quitar_seleccion_oriundo(){
   if(filtro_oriundo == true)
     {
+      cant_filtros_normales--
+      console.log("cant_filtros_normales: " ,cant_filtros_normales)
       cant_filtros--
+      console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
       console.log("cant_filtros: " ,cant_filtros)
       filtro_oriundo = false
       oriundo = document.getElementById("desplegable_oriundo")
@@ -91,17 +157,94 @@ function quitar_seleccion_oriundo(){
     }
 }
 
+function quitar_seleccion_sentencias(){
+  document.getElementById("por_obligaciones").checked = false
+  document.getElementById("penal_cbx").checked = false
+  document.getElementById("no_tiene").checked = false
+  let inputs1 = document.getElementsByName("cant_senten")
+  for (let i= 0 ; i < inputs1.length; i++){
+    inputs1[i].checked = false
+  }
+  let inputs2 = document.getElementsByName("cant_senten_oblig")
+  for (let i= 0 ; i < inputs2.length; i++){
+    inputs2[i].checked = false
+  }
+  
+  let inputs3 = document.getElementsByName("opc_mat_demanda")
+  for (let i= 0 ; i < inputs3.length; i++){
+    inputs3[i].checked = false
+  }
+
+  
+}
+
+
 //function onclick_
+function togglePenal_opciones(element){
+  if(element.checked == true){
+    if (document.getElementById("no_tiene").checked == true){
+      document.getElementById("no_tiene").checked = false
+    }
+    mostrar = document.getElementById("div_opc_cant_sentencias")
+    mostrar.style = ""
+  } 
+  else if (element.checked == false){
+    let mostrar = document.getElementById("div_opc_cant_sentencias")
+    mostrar.style = "display:none"
+    let inputs1 = document.getElementsByName("cant_senten")
+    for (let i= 0 ; i < inputs1.length; i++){
+      inputs1[i].checked = false
+    }
+  }
+}
+function toggleObligacion_opciones(element){
+  if(element.checked == true){
+    if (document.getElementById("no_tiene").checked == true){
+      document.getElementById("no_tiene").checked = false
+    }
+    let mostrar = document.getElementById("div_opc_obligaciones")
+    mostrar.style = ""
+  } 
+  else if (element.checked == false){
+    let mostrar = document.getElementById("div_opc_obligaciones")
+    mostrar.style = "display:none"
+    let inputs2 = document.getElementsByName("cant_senten_oblig")
+    for (let i= 0 ; i < inputs2.length; i++){
+      inputs2[i].checked = false
+    }
+    
+    let inputs3 = document.getElementsByName("opc_mat_demanda")
+    for (let i= 0 ; i < inputs3.length; i++){
+      inputs3[i].checked = false
+    }
+  }
+}
+
+function noTiene_opcion(element){
+  if (element.checked == true){
+    let  mostrar1 = document.getElementById("div_opc_cant_sentencias")
+    let mostrar2 = document.getElementById("div_opc_obligaciones")
+    mostrar1.style = "display:none"
+    mostrar2.style = "display:none"
+    document.getElementById("por_obligaciones").checked = false
+    document.getElementById("penal_cbx").checked = false
+  }
+}
+
 
 function onclick_sentencias(){
   filtro_sentencia = true
   cant_filtros++
+  
+
   console.log("cant_filtros: " ,cant_filtros)
 }
 function onchange_org_politica(){
   if(filtro_organizacion == false){
     cant_filtros++
     filtro_organizacion = true
+    cant_filtros_normales++
+    console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
     console.log("cant_filtros: " , cant_filtros)
   }
 }
@@ -110,6 +253,8 @@ function onclick_br() {
   {
     filtro_bienesraices = true
     cant_filtros++
+    cant_filtros_ob++
+    console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
     console.log("cant_filtros: " ,cant_filtros)
   }
 }
@@ -117,6 +262,9 @@ function quitar_seleccion_renuncia(){
   if(filtro_renuncias == true){
     let inputs = document.getElementsByName("cantidad_renuncia")
     cant_filtros--
+    cant_filtros_ob--
+    console.log("cant_filtros_ob: ", cant_filtros_ob)
+    console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
     console.log("cant_filtros: " ,cant_filtros)
     filtro_renuncias = false
     for(let i = 0 ; i < inputs.length; i++){
@@ -138,11 +286,18 @@ if(filtro_renuncias == false)
 function onclick_edad(){
   if(filtro_edad == false){
     filtro_edad = true
+    cant_filtros_ob++
+    console.log("cant_filtros_new",cant_filtros_ob+cant_filtros_normales)    
     cant_filtros++
     console.log("cant_filtros: " ,cant_filtros)
+    
+
+
 
   }
 }
+
+
 
 /*Onclick oriundo*/
 function onclick_oriundo(){
@@ -150,6 +305,9 @@ function onclick_oriundo(){
 
   if(filtro_oriundo == false){
     filtro_oriundo = true
+    cant_filtros_normales++
+    console.log("cant_filtros_normales: ", cant_filtros_normales)
+    console.log("cant_filtros_new: ",cant_filtros_ob+cant_filtros_normales)    
     cant_filtros++
     console.log("cant_filtros: " ,cant_filtros)
 
@@ -195,16 +353,7 @@ function get_range_edad(){
   }
 } 
 
-function get_nivel_academico(){
-  console.log("get_nivel_academico")
 
-  let nivel_academico = document.getElementById("nivel_academico")
-  let nivel_academico_value = nivel_academico.options[nivel_academico.selectedIndex].value;
-
-  console.log(nivel_academico_value)   
-  
-  return nivel_academico_value
-}
 function button_filter(){
   get_nivel_academico()
   let rango_edad = get_range_edad()
@@ -216,13 +365,7 @@ function get_oriundo(){
 
 }
 
-function get_cargos_previos(){
-  //return anhio_servicio_asc.checked && anhio_servicio_desc.checked
-  let button_asc = document.getElementById("anhio_servicio_asc")
-  if (button_asc.checked != true){
-      console.log("NULO CHECKED")
-  }
-}
+
 
 
   
@@ -241,12 +384,7 @@ function renuncias(){
   return cantidad_renuncia_asc.checked && cantidad_renuncia_desc.checked 
 
 }
-function get_nivel_academico() {
-  let nivel_academico = document.getElementById("nivel_academico")
-  let nivel_academico_value = nivel_academico.options[nivel_academico.selectedIndex].value;
-  // location.replace('http://' + win_href + ':8000/elecciones/candidatos/estudios/' + nivel_academico_value)
-  return nivel_academico_value
-}
+
 
 function cargos_previos() {
   let anhio_servicios = document.getElementsByName("anhio_servicio");
