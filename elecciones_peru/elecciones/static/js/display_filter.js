a88634fe14a8d7f8681cb5f7447d33fe1ee5a0f5
Miunmn <esteban.principe@utec.edu.pe>
2021-01-31 23:55:30 -0500

    update project

--- elecciones_peru/elecciones/static/js/display_filter.js
+++ elecciones_peru/elecciones/static/js/display_filter.js
@@ -1,12 +1,26 @@
-
+/*
 function tipo_candiato(element){
     if(element.text == "Candidatos Congresales"){
+        let div3 = document.getElementById("filtros_organizaciones")
+        div3.style.display ="none"	
+        let div4 = document.getElementById("filtros_candidatos")
+        div4.style.display ="block"
+
+
+
         document.getElementById("distrito_congreso").style = ""
         document.getElementById("postula_presidentes").style = "display : none"
         document.getElementsByName("tipo_candidato_filter")[0].id = "congresales"
         console.log(document.getElementsByName("tipo_candidato_filter")[0].id)
     }
     else if (element.text == "Candidatos Presidenciales"){
+        let div3 = document.getElementById("filtros_organizaciones")
+        div3.style.display ="none"	
+        let div4 = document.getElementById("filtros_candidatos")
+        div4.style.display ="block"
+
+
+
         document.getElementById("distrito_congreso").style = "display : none"
         document.getElementById("postula_presidentes").style = ""
         document.getElementsByName("tipo_candidato_filter")[0].id = "presidenciales"
@@ -14,6 +28,12 @@ function tipo_candiato(element){
 
     }
     else if (element.text == "Candidatos Parlamento Andino"){
+        let div3 = document.getElementById("filtros_organizaciones")
+        div3.style.display ="none"	
+        let div4 = document.getElementById("filtros_candidatos")
+        div4.style.display ="block"
+
+
         document.getElementById("distrito_congreso").style = "display : none"
         document.getElementById("postula_presidentes").style = "display : none"
         document.getElementsByName("tipo_candidato_filter")[0].id = "parlamento"
@@ -23,5 +43,5 @@ function tipo_candiato(element){
     }
 }
 
-
+*/
 
