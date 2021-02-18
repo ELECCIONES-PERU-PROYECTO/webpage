349ee18bebfcdca46e4d70be2c0932a44d567645
gracenikole <grace.alvarado@utec.edu.pe>
2021-02-18 18:27:08 -0500

    :frog: si

--- elecciones_peru/elecciones_peru/settings.py
+++ elecciones_peru/elecciones_peru/settings.py
@@ -28,7 +28,8 @@ DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1, localhost, elegimos.pe").split(",")
 
