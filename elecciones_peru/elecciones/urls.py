facb3d61aa30f943612e6dc19cb33fad8b4a84ef
gracenikole <grace.alvarado@utec.edu.pe>
2021-02-18 18:33:42 -0500

    :frog: url cambiada

--- elecciones_peru/elecciones/urls.py
+++ elecciones_peru/elecciones/urls.py
@@ -5,9 +5,9 @@ app_name = 'elecciones'
 urlpatterns = [
   path('', views.mainpage),
   path('analisis-graficos', views.analisisGraficos),
-  path('filtros', views.candidatos),
+  path('busqueda', views.candidatos),
   path('candidato/hojadevida/<str:dni_hoja_de_vida>/<str:cargo_postula_dato>', views.hojadevida_by_dni),
-  path('filtros/<str:nivel_academico>/<str:cargos_previos_order>/<str:orden_cant_sentencia>/<str:orden_cant_sentencia_oblig>/<str:mat_demanda>/<str:no_tiene_val>/<str:orden_cant_ingreso>/<str:orden_cant_inmueble>/<str:orden_valor_inmueble>/<str:orden_cant_mueble>/<str:orden_valor_mueble>/<str:orden_renuncias>/<str:rango_edad_val>/<str:nac_per_si>/<str:nac_per_no>/<str:departamento_nacimiento>/<str:cargo_postula>/<str:org_politica>/<str:dist_electoral>/<str:tipo_candidato_>', views.filter_function),
-  path('filtros/<str:filtro_id>/<str:info_extra>/<str:orden>', views.filter_function_orga),
+  path('busqueda/<str:nivel_academico>/<str:cargos_previos_order>/<str:orden_cant_sentencia>/<str:orden_cant_sentencia_oblig>/<str:mat_demanda>/<str:no_tiene_val>/<str:orden_cant_ingreso>/<str:orden_cant_inmueble>/<str:orden_valor_inmueble>/<str:orden_cant_mueble>/<str:orden_valor_mueble>/<str:orden_renuncias>/<str:rango_edad_val>/<str:nac_per_si>/<str:nac_per_no>/<str:departamento_nacimiento>/<str:cargo_postula>/<str:org_politica>/<str:dist_electoral>/<str:tipo_candidato_>', views.filter_function),
+  path('busqueda/<str:filtro_id>/<str:info_extra>/<str:orden>', views.filter_function_orga),
   # path('xde', views.test),
 ]
