7759d1018a6d57def973ba1991df04d8db3b501a
Alexandra-SR <alexandra.shulca@utec.edu.pe>
2021-03-09 19:22:27 -0500

    familiaAli

--- elecciones/views.py
+++ elecciones/views.py
@@ -239,7 +239,6 @@ def filter_function(request, nivel_academico, cargos_previos_order, orden_cant_s
   ]
 
   if(lista_valores[4][0:lista_valores[4].rfind("-")] == "FamiliaAli"):
-    print(">>>>>>>>>>>>>>>>>>>>>><<<<< Entro a familia")
     order = lista_valores[4][lista_valores[4].rfind("-"):len(lista_valores[4]) :+1]
     lista_valores[4] = "FAMILIA / ALIMENTARIA" + order
 
