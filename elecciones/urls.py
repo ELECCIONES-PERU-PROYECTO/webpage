2d471c3c590b44a8e36c4de1832cf125acfec219
gracenikole <grace.alvarado@utec.edu.pe>
2021-03-11 20:11:43 -0500

    :cow: css IA y yt mb

--- elecciones/urls.py
+++ elecciones/urls.py
@@ -5,7 +5,6 @@ app_name = 'elecciones'
 urlpatterns = [
   path('', views.mainpage, name="mainpage"),
   path('analisis-graficos', views.analisisGraficos, name="analisisGraficos"),
-  path('analisis-graficos-presi', views.analisisGraficosPresi, name='analisisGraficosPresi'),
   path('candidato/hojadevida/<str:dni_hoja_de_vida>/<str:cargo_postula_dato>', views.hojadevida_by_dni, name="hojadevida_by_dni"),
   path('inteligencia-articial',views.iaDisplay, name="iaDisplay"),
   path('nosotros', views.nosotros, name="nosotros"),
