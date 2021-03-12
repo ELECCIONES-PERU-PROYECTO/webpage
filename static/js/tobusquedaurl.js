9f1a03efd48e16a806dcc04c572473b54bb6ef58
gracenikole <grace.alvarado@utec.edu.pe>
2021-03-12 00:28:52 -0500

    :cow: send badges from graphics

--- static/js/tobusquedaurl.js
+++ static/js/tobusquedaurl.js
@@ -30,7 +30,6 @@ function tipo_candidato_select(select_)
 
 function busqueda_url(id_)
 {   
-
     if(organizacion_politica=="" || tipo_candidato=="") {
           setTimeout(function(){
             UIkit.notification({
@@ -42,29 +41,36 @@ function busqueda_url(id_)
       }
     if(id_=="edad_busqueda")
     {
+        sessionStorage.setItem('data_filtros_seleccionados', JSON.stringify([13]))
         window.location = "/busqueda?tipo_filter="+tipo_candidato+"&rango_edad="+info_extra+"-1&org_politica="+organizacion_politica+"-2"
     }
     else if(id_=="cargo_previos_busqueda")
     {
+        sessionStorage.setItem('data_filtros_seleccionados', JSON.stringify([2]))
         window.location = "/busqueda?tipo_filter="+tipo_candidato+"&anhio_servicio=DESC-1&org_politica="+organizacion_politica+"-2"
     }
     else if (id_=="renuncia_busqueda"){
+        sessionStorage.setItem('data_filtros_seleccionados', JSON.stringify([12]))
         window.location ="/busqueda?tipo_filter="+tipo_candidato+"&cantidad_renuncia=DESC-1&org_politica="+organizacion_politica+"-2"
     }
     else if (id_=="sentencias_busqueda"){
+        sessionStorage.setItem('data_filtros_seleccionados', JSON.stringify([3, 4]))
         window.location = "/busqueda?tipo_filter="+tipo_candidato+"&ifsentencias=si&cant_senten=DESC-1&cant_senten_oblig=DESC-2&org_politica="+organizacion_politica+"-3"
     }
     else if (id_=="ingreso_busqueda"){
+        sessionStorage.setItem('data_filtros_seleccionados', JSON.stringify([7]))
         window.location = "/busqueda?tipo_filter="+tipo_candidato+"&cant_ingreso=DESC-1&org_politica="+organizacion_politica+"-2"
     }
     else if (id_=="inmueble_busqueda"){
+        sessionStorage.setItem('data_filtros_seleccionados', JSON.stringify([8]))
         window.location = "/busqueda?tipo_filter="+tipo_candidato+"&cant_inmuebles=DESC-1&org_politica="+organizacion_politica+"-2"
     }
     else if (id_=="mueble_busqueda"){
+        sessionStorage.setItem('data_filtros_seleccionados', JSON.stringify([10]))
         window.location = "/busqueda?tipo_filter="+tipo_candidato+"&cant_muebles=DESC-1&org_politica="+organizacion_politica+"-2"
     }
     else if (id_=="academico_busqueda"){
+        sessionStorage.setItem('data_filtros_seleccionados', JSON.stringify([1]))
         window.location = "/busqueda?tipo_filter="+tipo_candidato+"&nivel_academico="+info_extra+"-1&org_politica="+organizacion_politica+"-2"
     }
-
 }
